51797103a071642d5af711d931164b831c86bdb5
hejinjing <15373661380@qq.com>
2023-02-13 12:55:50 +0800

    整理代码

--- option.h
+++ option.h
@@ -19,161 +19,109 @@ class option_base
 		virtual const std::string &description() const=0;
 		virtual std::string short_description() const=0;
 };
-
 class option_without_value : public option_base  //无value 的选项
 {
 	public: //定义构造函数
-		option_without_value(const std::string &name,
-				char short_name,
-				const std::string &desc)
+		option_without_value(const std::string &name, char short_name, const std::string &desc)
 			:nam(name), snam(short_name), desc(desc), has(false){ }
 		~option_without_value(){}
 		//实现访问方法
 		bool has_value() const { return false; }
-
 		bool set(){	// 无value, 设置返回真,并置位has 为true(表示已经设置过）
 			has=true;
 			return true;
 		}
-
 		bool set(const std::string &){ //设置字符串就不必了
 			return false;
 		}
-
 		bool has_set() const {
 			return has;
 		}
-
 		bool valid() const{
 			return true;
 		}
-
-		bool must() const{ // 无值,must 为false
-			return false;
-		}
-
-		const std::string &name() const{
-			return nam;
-		}
-
-		char short_name() const{
-			return snam;
-		}
-
-		const std::string &description() const {
-			return desc;
-		}
-
-		std::string short_description() const{ //short_description 给nam
+		bool must() const{ return false;} // 无值的option,must 为false
+		const std::string &name() const{return nam;}
+		char short_name() const{return snam;}
+		const std::string &description() const {return desc;}
+		std::string short_description() const{ //short_description 是nam
 			return "--"+nam;
 		}
-
 	private: //定义私有变量
 		std::string nam;
 		char snam;
 		std::string desc;
 		bool has;
 };
-
 template <class T>
 class option_with_value : public option_base 	//带值的选项, 值的类型为T, 模板类
 {
-	public: //构造函数
-		option_with_value(const std::string &name,
-				char short_name,
-				bool need,
-				const T &def,
-				const std::string &desc)
-			: nam(name), snam(short_name), need(need), has(false)
-			  , def(def), actual(def) 
+public: //构造函数
+	option_with_value(const std::string &name, char short_name, bool need, const T &defv, const std::string &desc)
+		: nam(name), snam(short_name), need(need), has(false), defv(defv), actual(defv) 
 	{
 		this->desc=full_description(desc);
 	}
-		~option_with_value(){}
-		//实现访问方法
-		const T &get() const {
-			return actual;
+	~option_with_value(){}
+	//实现访问方法
+	const T &get() const {return actual;}
+	bool has_value() const { return true; }
+	bool set(){return false; }//设置空,返回false
+	bool set(const std::string &value){ //把数值用参数传进来
+		try{
+			actual=read(value); //读取其值保存到actual, read 是类型T提供的read函数,由继承类实现,后面只定义了虚接口
+			has=true;
 		}
-
-		bool has_value() const { return true; }
-
-		bool set(){
+		catch(const std::exception &e){
 			return false;
 		}
+		return true;
+	}
+	bool has_set() const{return has;}
+	bool valid() const{
+		if (need && !has) return false;
+		return true;
+	}
+	bool must() const{return need;}
+	const std::string &name() const{return nam;}
+	char short_name() const{return snam;}
+	const std::string &description() const {return desc;}
+	std::string short_description() const{
+		return "--"+nam+"="+CAST::readable_typename<T>(); // 返回名称加类型
+	}
+protected:
+	std::string full_description(const std::string &desc){
+		return
+			desc+" ("+CAST::readable_typename<T>()+
+			(need?"":" [="+CAST::default_value<T>(defv)+"]")  // 返回描述加类型及默认值
+			+")";
+	}
 
-		bool set(const std::string &value){ //把数值用参数传进来
-			try{
-				actual=read(value); //读取其值保存到actual, read 是类型T提供的read函数,由继承类实现,后面只定义了虚接口
-				has=true;
-			}
-			catch(const std::exception &e){
-				return false;
-			}
-			return true;
-		}
+	virtual T read(const std::string &s)=0;
+	//定义私有变量
+	std::string nam;
+	char snam;
+	bool need;
+	std::string desc;
 
-		bool has_set() const{
-			return has;
-		}
-
-		bool valid() const{
-			if (need && !has) return false;
-			return true;
-		}
-
-		bool must() const{
-			return need;
-		}
-
-		const std::string &name() const{
-			return nam;
-		}
-
-		char short_name() const{
-			return snam;
-		}
-
-		const std::string &description() const {
-			return desc;
-		}
-
-		std::string short_description() const{
-			return "--"+nam+"="+CAST::readable_typename<T>(); // 返回名称加类型
-		}
-
-	protected:
-		std::string full_description(const std::string &desc){
-			return
-				desc+" ("+CAST::readable_typename<T>()+
-				(need?"":" [="+CAST::default_value<T>(def)+"]")  // 返回描述加类型及默认值
-				+")";
-		}
-
-		virtual T read(const std::string &s)=0;
-		//定义私有变量
-		std::string nam;
-		char snam;
-		bool need;
-		std::string desc;
-
-		bool has;
-		T def;
-		T actual;
+	bool has;
+	T defv;
+	T actual;
 };
 // 该测试中会生成2种代码,
 //1. <int, range_reader<int> >
 //2. <std::string, default_reader<std::string> >
-template <class T, class F>
+template <class T, class R>
 class option_with_value_with_reader : public option_with_value<T> 
 {
 public: //包含reader 的选项构造
   option_with_value_with_reader(const std::string &name,
 								char short_name,
 								bool need,
-								const T def,
+								const T defv,
 								const std::string &desc,
-								F reader)
-	: option_with_value<T>(name, short_name, need, def, desc), aReader(reader)
+								R reader)
+	: option_with_value<T>(name, short_name, need, defv, desc), aReader(reader)
   {
   }
 
@@ -183,7 +131,6 @@ private:
 	return aReader(s); //返回一个reader 对象，其参数是string s
   }
 
-		F aReader;	// 有一个对象reader
+		R aReader;	// 有一个对象reader
 };
-
 #endif
--- reader.h
+++ reader.h
@@ -1,37 +1,39 @@
 #pragma once
-#include <vector>
 #include <string>
 #include "myexception.h"
-
-//模板类
-template <class T>
-class CDefault_reader
-{ 
-public:
-	//default_reader 不包含任何成员变量，只有一个()操作函数，返回T 类型数值
-	T operator()(const std::string &str){ //其运算符()的意义为把string转换为T类型
-		return CAST::lexical_cast<T>(str);
-	}
-};
-
-//模板类
-template <class T>
-class CRange_reader
-{ //定义模板类range_reader
-public:
-	CRange_reader(const T &low, const T &high): low(low), high(high) {} 
-	T operator()(const std::string &s) const {
-		T ret=CDefault_reader<T>()(s);
-		if (!(ret>=low && ret<=high)) throw cmdline_error("CRange_error"); //返回T 值或者抛出异常
-		return ret;
-	}
-	private:
-	T low, high;
-};
+template <class T> class CRange_reader;
+template <class T> class CDefault_reader;
 
 //模板函数
 template <class T>
-CRange_reader<T> range(const T &low, const T &high) //定义一个函数range,返回range_reader<T> 类对象
-{ //返回的是一个对象
-	return CRange_reader<T>(low, high);
+CRange_reader<T> range(const T &low, const T &high)
+{
+	return CRange_reader<T>(low,high);
 }
+
+template <class T>
+class CRange_reader
+{
+public:
+	CRange_reader(const T&l, const T&h): low(l),high(h){}
+	T operator()(const std::string &s)const
+	{
+		T ret=CDefault_reader<T>()(s);
+		if(!(ret>=low && ret<=high)) throw cmdline_error("CRange_error");
+		return ret;
+	}
+	T low;
+	T high;
+};
+
+template <class T>
+class CDefault_reader
+{
+public:
+	//	CDefault_reader(const std::string &s) 连显示默认构造都没有	
+	T operator()(const std::string& s)
+	{
+		return CAST::lexical_cast<T>(s);
+	}
+
+};
